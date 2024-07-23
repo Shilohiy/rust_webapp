fe73efcc9b685891c77927af85e8a89358c3a692
Laplace <shilohy_brandy@163.com>
2024-07-23 14:23:12 +0800

    ALL OK

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -5,16 +5,14 @@ use std::str;
 fn main() {
     let mut stream = TcpStream::connect("localhost:3000").unwrap();
     println!("Connected to the server!");
-    // Send a message to the server
     let message = "Hello from the client!";
     stream.write(message.as_bytes()).unwrap();
 
-    // Receive a message from the server
     let mut buffer = vec![0; message.len()];
     stream.read(&mut buffer).unwrap();
 
-    println!(
+    println!(  
         "Received from server: {:?}",
-        str::from_utf8(&buffer).unwrap()
+         str::from_utf8(&buffer).unwrap()
     );
 }
